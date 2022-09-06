dc3c67a1a7b0faca113ded3ed362c833b2e8fc1f
Laura <laurakovacic@outlook.com>
2022-09-06 14:22:12 +0200

    push project files for Project 1

---
+++ 01_WeSplit/01_WeSplit/_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_WeSplitApp.swift
+//  01_WeSplit
+//
+//  Created by Laura on 06.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
