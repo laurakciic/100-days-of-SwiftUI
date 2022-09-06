11cd61f7c4881ff351735bc64d9d6cc9fae88ad7
Laura <laurakovacic@outlook.com>
2022-09-07 00:49:41 +0200

    make UI

--- Challenge1_Conversions/Challenge1_Conversions/ContentView.swift
+++ Challenge1_Conversions/Challenge1_Conversions/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var input = 0.0
+    @State private var inputUnit = "days"
+    @State private var outputUnit   = "hours"
+    
+    @FocusState private var amountFocused: Bool
+    
+    let units = ["seconds", "minutes", "hours", "days"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter amount", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountFocused)
+                    
+                } header: {
+                    Text("convert")
+                }
+                
+                Section {
+                    Picker("Time options", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("from")
+                }
+                
+                Section {
+                    Picker("Time options", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("to")
+                }
+                
+                Section {
+                    Text("\(input) \(inputUnit) equals ... \(outputUnit).")
+                } header: {
+                    Text("conversion")
+                }
+            }
+            .navigationTitle("Time Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
