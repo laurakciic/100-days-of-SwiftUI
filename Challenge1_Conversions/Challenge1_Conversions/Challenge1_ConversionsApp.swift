be5cdd589b9fb630505300eb6d3bae9c97b71bbf
Laura <laurakovacic@outlook.com>
2022-09-07 00:48:57 +0200

    push project files for Challenge 1

---
+++ Challenge1_Conversions/Challenge1_Conversions/Challenge1_ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1_ConversionsApp.swift
+//  Challenge1_Conversions
+//
+//  Created by Laura on 07.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct Challenge1_ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
