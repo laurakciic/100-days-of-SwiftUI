71a1ecae679621dff6da14189f887b36478fee53
Laura <laurakovacic@outlook.com>
2022-09-07 11:42:03 +0200

    push project files for Project2 - Guess The Flag

---
+++ 02_Guess_The_Flag/02_Guess_The_Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  02_Guess_The_Flag
+//
+//  Created by Laura on 07.09.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 02_Guess_The_Flag/02_Guess_The_Flag/_2_Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_Guess_The_FlagApp.swift
+//  02_Guess_The_Flag
+//
+//  Created by Laura on 07.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _2_Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
