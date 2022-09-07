5a57a7f24d74b8d3c6e881157c9aeee73cda6088
Laura <laurakovacic@outlook.com>
2022-09-07 16:40:43 +0200

    push project files for Project3 & add git assets for P2

---
+++ 03_Views&Modifiers/03_Views&Modifiers/_3_Views_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_Views_ModifiersApp.swift
+//  03_Views&Modifiers
+//
+//  Created by Laura on 07.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _3_Views_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
