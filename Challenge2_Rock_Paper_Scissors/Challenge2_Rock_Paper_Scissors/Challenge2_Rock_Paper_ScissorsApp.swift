6d0c62a7c483e26bf43bf4cb8a6843aa3608692f
Laura <laurakovacic@outlook.com>
2022-09-08 00:14:39 +0200

    push project files for Challenge 2

---
+++ Challenge2_Rock_Paper_Scissors/Challenge2_Rock_Paper_Scissors/Challenge2_Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2_Rock_Paper_ScissorsApp.swift
+//  Challenge2_Rock_Paper_Scissors
+//
+//  Created by Laura on 08.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct Challenge2_Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge2_Rock_Paper_Scissors/Challenge2_Rock_Paper_Scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge2_Rock_Paper_Scissors
+//
+//  Created by Laura on 08.09.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
