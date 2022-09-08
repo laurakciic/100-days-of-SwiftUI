50ba8140105a8a8ac394fde475f428a505532ac6
Laura <laurakovacic@outlook.com>
2022-09-08 22:39:09 +0200

    push project files for P6

---
+++ 06_Animations/06_Animations/_6_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_AnimationsApp.swift
+//  06_Animations
+//
+//  Created by Laura on 08.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _6_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
