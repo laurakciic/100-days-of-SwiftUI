bf670cad788ece542b23569b026401d76ed870b0
Laura <laurakovacic@outlook.com>
2022-09-08 14:17:11 +0200

    push project files for Project 5

---
+++ 05_WordScramble/05_WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  05_WordScramble
+//
+//  Created by Laura on 08.09.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 05_WordScramble/05_WordScramble/_5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_WordScrambleApp.swift
+//  05_WordScramble
+//
+//  Created by Laura on 08.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
