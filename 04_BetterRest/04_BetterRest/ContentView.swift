c3de9f1cd95cd86fe40a566d7210f62ef7ce92b5
Laura <laurakovacic@outlook.com>
2022-09-08 12:50:06 +0200

    create & train ML model + create basic layout

--- 04_BetterRest/04_BetterRest/ContentView.swift
+++ 04_BetterRest/04_BetterRest/ContentView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
+    @State private var coffeeAmout = 1
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmout == 1 ? "1 cup" : "\(coffeeAmout) cups", value: $coffeeAmout, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }
+    }
+    
+    private func calculateBedtime() {
+        
     }
 }
 
