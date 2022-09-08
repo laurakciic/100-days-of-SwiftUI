fcac0045264034aa3fd0061ad4a3c307926c084d
Laura <laurakovacic@outlook.com>
2022-09-08 11:54:19 +0200

    push project files for Project 4

---
+++ 04_BetterRest/04_BetterRest/_4_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_BetterRestApp.swift
+//  04_BetterRest
+//
+//  Created by Laura on 08.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _4_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
