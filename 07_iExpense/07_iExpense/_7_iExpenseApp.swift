1348e652a63f48bb3dfebd6e6eb8fb189823b00a
Laura <laurakovacic@outlook.com>
2022-09-09 12:55:20 +0200

    push project files for P7

---
+++ 07_iExpense/07_iExpense/_7_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_iExpenseApp.swift
+//  07_iExpense
+//
+//  Created by Laura on 09.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _7_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
