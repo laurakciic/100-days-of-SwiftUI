1540de9e35de0e81824f3920961b2e02d446aeab
Laura <laurakovacic@outlook.com>
2022-09-09 09:36:06 +0200

    add start screen UI

---
+++ Challenge3_Kids_Multiplication_Game/Challenge3_Kids_Multiplication_Game/Challenge3_Kids_Multiplication_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3_Kids_Multiplication_GameApp.swift
+//  Challenge3_Kids_Multiplication_Game
+//
+//  Created by Laura on 09.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct Challenge3_Kids_Multiplication_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge3_Kids_Multiplication_Game/Challenge3_Kids_Multiplication_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge3_Kids_Multiplication_Game
+//
+//  Created by Laura on 09.09.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
