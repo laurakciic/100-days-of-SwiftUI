f94b73b6e22ee41ac8a7a019a25fafad4115b872
Laura <laurakovacic@outlook.com>
2022-09-12 14:28:05 +0200

    using generics to load any kind of Codable data (more complex JSON)

---
+++ 08_Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  08_Moonshot
+//
+//  Created by Laura on 12.09.2022..
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
