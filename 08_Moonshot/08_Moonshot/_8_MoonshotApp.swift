253f7c64fbebbea8d538ea2cf900bcc9e1848aaa
Laura <laurakovacic@outlook.com>
2022-09-12 11:29:51 +0200

    push files for P8

---
+++ 08_Moonshot/08_Moonshot/_8_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_MoonshotApp.swift
+//  08_Moonshot
+//
+//  Created by Laura on 12.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _8_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
