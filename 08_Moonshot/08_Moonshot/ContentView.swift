1c39df331d213e2e16f2b63c984235094eb82cc9
Laura <laurakovacic@outlook.com>
2022-09-12 11:30:17 +0200

    resize images to fit the screen using GeometryReader

--- 08_Moonshot/08_Moonshot/ContentView.swift
+++ 08_Moonshot/08_Moonshot/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            Image("aldrin")
+                .resizable()                                                // make image scaleable & stretchable
+                .scaledToFit()
+                .frame(width: geo.size.width * 1)                           // 100% width
+                .frame(width: geo.size.width, height: geo.size.height)      // align image
+        }
     }
 }
 
