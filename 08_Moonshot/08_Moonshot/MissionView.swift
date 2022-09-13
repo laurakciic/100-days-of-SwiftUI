5c9500a27e61b2b6124af115060fabde34b8aeee
Laura <laurakovacic@outlook.com>
2022-09-13 10:23:09 +0200

    merge codable structs - match up data from two different JSON files for showing mission description details

--- 08_Moonshot/08_Moonshot/MissionView.swift
+++ 08_Moonshot/08_Moonshot/MissionView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct MissionView: View {
     
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let crew: [CrewMember]
     
     var body: some View {
         GeometryReader { geometry in
@@ -18,33 +24,91 @@ struct MissionView: View {
                     Image(mission.image)
                         .resizable()
                         .scaledToFit()
-                        .frame(maxWidth: geometry.size.width * 0.6)     // 60% of available screen width
-                        .padding()
+                        .frame(maxWidth: geometry.size.width * 0.6)         // 60% of available screen width
+                        .padding(.top)
                     
                     VStack(alignment: .leading) {
+                        Rectangle()                                         // divider
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+                        
                         Text("Mission Highlights")
                             .font(.title.bold())
                             .padding(.bottom, 5)
                         
                         Text(mission.description)
+                        
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+                        
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
                     }
-                    .padding(.horizontal)                               // stay away from edge of the screen
+                    .padding(.horizontal)                                   // stay away from edge of the screen
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(crew, id: \.role) { crewMember in
+                                NavigationLink {
+                                    Text("Astronaut details")
+                                } label: {
+                                    HStack {
+                                        Image(crewMember.astronaut.id)      // astronaut pictures
+                                            .resizable()
+                                            .frame(width: 104, height: 72)  // matches ratio of original
+                                            .clipShape(Capsule())
+                                            .overlay(
+                                                Capsule()
+                                                    .strokeBorder(.white, lineWidth: 1)  // border inside
+                                            )
+                                        
+                                        VStack(alignment: .leading) {
+                                            Text(crewMember.astronaut.name)
+                                                .foregroundColor(.white)
+                                                .font(.headline)
+                                            
+                                            Text(crewMember.role)
+                                                .foregroundColor(.secondary)
+                                        }
+                                    }
+                                    .padding(.horizontal)                   // space between each astronaut & also edges
+                                }
+                            }
+                        }
+                    }
                 }
-                .padding(.bottom)                                       // so it can scroll smoothly away from bottom
+                .padding(.bottom)                                           // so it can scroll smoothly away from bottom
             }
         }
         .navigationTitle(mission.displayName)
-        .navigationBarTitleDisplayMode(.inline)                         // second screen & we don't want big titles
+        .navigationBarTitleDisplayMode(.inline)                             // second screen & we don't want big titles
         .background(.darkBackground)
+    }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
     }
 }
 
 struct MissionView_Previews: PreviewProvider {
     
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
             .preferredColorScheme(.dark)
     }
 }
