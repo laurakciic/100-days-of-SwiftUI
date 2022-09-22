9e7cab42e7e49352d6e53fd4cecbe3e52d5651f3
Laura <laurakovacic@outlook.com>
2022-09-22 11:13:16 +0200

    push files for P10

---
+++ 10_CupcakeCorner/10_CupcakeCorner/_0_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_CupcakeCornerApp.swift
+//  10_CupcakeCorner
+//
+//  Created by Laura on 22.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _0_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
