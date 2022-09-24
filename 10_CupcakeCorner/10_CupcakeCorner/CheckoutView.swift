ec578701a832f49d51d3174b3b3887c4b5709c1f
Laura <laurakovacic@outlook.com>
2022-09-24 11:04:49 +0200

    P: check for a valid address

---
+++ 10_CupcakeCorner/10_CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,22 @@
+//
+//  CheckoutView.swift
+//  10_CupcakeCorner
+//
+//  Created by Laura on 24.09.2022..
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
