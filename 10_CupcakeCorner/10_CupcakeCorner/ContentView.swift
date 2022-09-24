d69762f1ed41aa0762c02922198b7face2981b14
Laura <laurakovacic@outlook.com>
2022-09-24 10:26:27 +0200

    E: load an image from remote server

--- 10_CupcakeCorner/10_CupcakeCorner/ContentView.swift
+++ 10_CupcakeCorner/10_CupcakeCorner/ContentView.swift
@@ -7,51 +7,32 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackID: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
     
     var body: some View {
-        List(results, id: \.trackID) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()                        // might take some time
-        }
-    }
-    
-    func loadData() async {
-        // 1. create URL we want to read
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")
-        else {
-            print("Invalid URL")
-            return
-        }
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
         
-        // 2. fetch data for URL
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url) // return type tuple, _ to toss away metadata
-            
-            // 3. decode result of that data into response struct
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results   // assign results from Response to results in View
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+//            image
+//                .resizable()
+//                .scaledToFit()
+//        } placeholder: {
+//            ProgressView()                          // loading spinner
+//        }
+//        .frame(width: 200, height: 200)
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("Error loading image.")
+            } else {
+                ProgressView()
             }
-        } catch {
-            print("Invalid data")
         }
+        .frame(width: 200, height: 200)
     }
 }
 
