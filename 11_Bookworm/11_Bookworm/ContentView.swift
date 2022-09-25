063445def81708ee211fb962995138fc5a3e35b6
Laura <laurakovacic@outlook.com>
2022-09-25 09:47:55 +0200

    P: create books with CoreData & add a custom star rating component

--- 11_Bookworm/11_Bookworm/ContentView.swift
+++ 11_Bookworm/11_Bookworm/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc    // to store view context for coordinator model so we can use that later to delete books
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>  // reading saved books so we can show updates on UI
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Text("Cound: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
     }
 }
 
