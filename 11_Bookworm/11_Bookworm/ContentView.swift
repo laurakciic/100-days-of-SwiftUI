f93a38d63403744baa0d1af50ef433107421d1e6
Laura <laurakovacic@outlook.com>
2022-09-25 08:43:32 +0200

    P: prepare CoreData

--- 11_Bookworm/11_Bookworm/ContentView.swift
+++ 11_Bookworm/11_Bookworm/ContentView.swift
@@ -8,30 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    // fetch req with no sorting then put it in a property students with a type FetchedResults of a type Student
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
-    
     var body: some View {
-        VStack {
-            List(students) { student in
-                Text(student.name ?? "Uknown")
-            }
-            
-            Button("Add") {
-                let firstNames = ["Laura", "Luka", "Buhtlica", "Simba"]
-                let lastName  = ["Roncevic"]
-                
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastName[0]
-            
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
