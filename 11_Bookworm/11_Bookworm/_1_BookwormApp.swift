69bbb1955af95c70ca42b60f7c706a240d1bc0ae
Laura <laurakovacic@outlook.com>
2022-09-25 08:40:33 +0200

    E: combine Core Data & SwiftUI

--- 11_Bookworm/11_Bookworm/_1_BookwormApp.swift
+++ 11_Bookworm/11_Bookworm/_1_BookwormApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct _1_BookwormApp: App {
+    // makes instance od data controller and keeps it alive
+    @StateObject private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                // inject loaded data into environment
+                // managed object context: live versions of data (that exist in memory)
+                // view context lets us work with data in memory - load it once, work with the memory, modify & then write out only when ready 
+                .environment(\.managedObjectContext, dataController.containter.viewContext)
         }
     }
 }
