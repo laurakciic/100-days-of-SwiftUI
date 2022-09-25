d1f7982cc6af00928fd79124ee20e331023884df
Laura <laurakovacic@outlook.com>
2022-09-25 07:50:35 +0200

    push files for P11

---
+++ 11_Bookworm/11_Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  11_Bookworm
+//
+//  Created by Laura on 25.09.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 11_Bookworm/11_Bookworm/_1_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_BookwormApp.swift
+//  11_Bookworm
+//
+//  Created by Laura on 25.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct _1_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
