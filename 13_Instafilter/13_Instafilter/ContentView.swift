33751cafa276ae95705fbd1ff5f333574c57139c
Laura <laurakovacic@outlook.com>
2022-09-27 06:54:16 +0200

    push files for P13

---
+++ 13_Instafilter/13_Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  13_Instafilter
+//
+//  Created by Laura on 27.09.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
