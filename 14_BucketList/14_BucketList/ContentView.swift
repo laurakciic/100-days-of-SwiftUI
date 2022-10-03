173ceef94b5e8dfa166a899f49ec4f7cc27a32c6
Laura <laurakovacic@outlook.com>
2022-10-03 12:36:22 +0200

    push files for P14

---
+++ 14_BucketList/14_BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  14_BucketList
+//
+//  Created by Laura on 03.10.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 14_BucketList/14_BucketList/_4_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_BucketListApp.swift
+//  14_BucketList
+//
+//  Created by Laura on 03.10.2022..
+//
+
+import SwiftUI
+
+@main
+struct _4_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
