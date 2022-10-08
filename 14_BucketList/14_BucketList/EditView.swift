93be8e1f6a2c065b7d1645fd529e50d4f0fc7109
Laura <laurakovacic@outlook.com>
2022-10-08 14:39:16 +0200

    P: sort wikipedia results with Comparable + show description

--- 14_BucketList/14_BucketList/EditView.swift
+++ 14_BucketList/14_BucketList/EditView.swift
@@ -39,7 +39,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ")
-                            + Text("Page description here")
+                            + Text(page.description)
                                 .italic()
                         }
                     case .failed:
@@ -87,7 +87,7 @@ struct EditView: View {
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             let items = try JSONDecoder().decode(Result.self, from: data)           // decode fetched data
-            pages = items.query.pages.values.sorted { $0.title < $1.title }         // pull decoded data into object, sorted alphabetically (convert arr values to our pages arr)
+            pages = items.query.pages.values.sorted()                               // pull decoded data into object, sorted alphabetically (convert arr values to our pages arr)
             loadingState = .loaded
         } catch {
             loadingState = .failed
--- 14_BucketList/14_BucketList/Result.swift
+++ 14_BucketList/14_BucketList/Result.swift
@@ -15,8 +15,16 @@ struct Query: Codable {
     let pages: [Int: Page]              // pages dict - pageid as key (Wikipedia's internal identifier), wikipedia pages as values
 }
 
-struct Page: Codable {
+struct Page: Codable, Comparable {
     let pageid: Int
     let title: String
-    let terms: [String: [String]]?      // dictionary with strings as keys + array of optional strings as values
+    let terms: [String: [String]]?      // dictionary with strings as keys + array of strings as values
+    
+    var description: String {
+        terms?["description"]?.first ?? "No further information"    // attempt to read the description key and first item in that array
+    }
+    
+    static func <(lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
 }
