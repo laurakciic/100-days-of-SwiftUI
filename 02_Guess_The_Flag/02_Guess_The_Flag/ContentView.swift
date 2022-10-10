f71d3bc8cb4c6043888e497b152522a5ce950759
Laura <laurakovacic@outlook.com>
2022-10-10 14:33:46 +0200

    P: add accessibility feature for voice over in Guess The Flag

--- 02_Guess_The_Flag/02_Guess_The_Flag/ContentView.swift
+++ 02_Guess_The_Flag/02_Guess_The_Flag/ContentView.swift
@@ -19,6 +19,21 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "UK", "Italy", "Nigeria", "Poland", "Russia", "Spain", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
+    // for accessibility purposes
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     struct FlagImage: View {
         var country: String
         
@@ -62,6 +77,9 @@ struct ContentView: View {
                             flagTapped(number)
                         } label: {
                             FlagImage(country: countries[number])
+                                // give me the item from countries arr at this position
+                                // give me labels dictionary with this key
+                                .accessibilityLabel(labels[countries[number], default: "Uknown flag"])
                         }
                     }
                 }
