6ea716497d35fe995168003c7f504475d469a129
Laura <laurakovacic@outlook.com>
2022-10-10 12:52:24 +0200

    push files for P15

---
+++ 15_Accessibility/15_Accessibility/_5_AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_AccessibilityApp.swift
+//  15_Accessibility
+//
+//  Created by Laura on 10.10.2022..
+//
+
+import SwiftUI
+
+@main
+struct _5_AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
