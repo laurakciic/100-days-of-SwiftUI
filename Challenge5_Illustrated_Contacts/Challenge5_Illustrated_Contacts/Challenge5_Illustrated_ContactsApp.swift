711c6d74d735b027eac9dac43e0fb0e5ce3be4f8
Laura <laurakovacic@outlook.com>
2022-10-10 20:58:32 +0200

    push files for challenge 5

---
+++ Challenge5_Illustrated_Contacts/Challenge5_Illustrated_Contacts/Challenge5_Illustrated_ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge5_Illustrated_ContactsApp.swift
+//  Challenge5_Illustrated_Contacts
+//
+//  Created by Laura on 10.10.2022..
+//
+
+import SwiftUI
+
+@main
+struct Challenge5_Illustrated_ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge5_Illustrated_Contacts/Challenge5_Illustrated_Contacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge5_Illustrated_Contacts
+//
+//  Created by Laura on 10.10.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
