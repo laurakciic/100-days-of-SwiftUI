d7911c89a7052c594b35ae897cc3b5e825b322f7
Laura <laurakovacic@outlook.com>
2022-10-10 13:54:20 +0200

    E: read the value of controls

--- 15_Accessibility/15_Accessibility/ContentView.swift
+++ 15_Accessibility/15_Accessibility/ContentView.swift
@@ -8,20 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var value = 10
     
     var body: some View {
-//        Image(decorative: "character")              // decorative - won't be read with voice over
-//            .accessibilityHidden(true)              // view invisible to the underlying voice over system
-        
         VStack {
-            Text("Your score is")
-            Text("1000")
-                .font(.title)
+            Text("Value: \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value += 1
+            }
         }
-//        .accessibilityElement(children: .combine)     // read them together
-        .accessibilityElement(children: .ignore)        // or just .accesibilityElement() - by default it will ignore children views
-        .accessibilityLabel("Your score is 1000")
+        .accessibilityElement()                     // everything read as one
+        .accessibilityLabel("Value")                // default label
+        .accessibilityValue(String(value))          // value inside element
+        .accessibilityAdjustableAction { direction in       // which direction did user try & adjust this thing - up/down
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+        }
     }
 }
 
