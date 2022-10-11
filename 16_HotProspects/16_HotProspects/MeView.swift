4dff8c5c75952be2c3a60823fce74d445736a944
Laura <laurakovacic@outlook.com>
2022-10-11 12:15:26 +0200

    P: build tab bar

---
+++ 16_HotProspects/16_HotProspects/MeView.swift
@@ -0,0 +1,20 @@
+//
+//  MeView.swift
+//  16_HotProspects
+//
+//  Created by Laura on 11.10.2022..
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
