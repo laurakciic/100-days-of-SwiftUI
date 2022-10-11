eeb77375244fba0d6f112eae800449044c543445
Laura <laurakovacic@outlook.com>
2022-10-11 09:06:07 +0200

    push files for P16

---
+++ 16_HotProspects/16_HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  16_HotProspects
+//
+//  Created by Laura on 11.10.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 16_HotProspects/16_HotProspects/_6_HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_HotProspectsApp.swift
+//  16_HotProspects
+//
+//  Created by Laura on 11.10.2022..
+//
+
+import SwiftUI
+
+@main
+struct _6_HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
