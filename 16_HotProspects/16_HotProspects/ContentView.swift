4a8ef557056181d69ba5af92a54d2a0d079c8f89
Laura <laurakovacic@outlook.com>
2022-10-11 10:07:28 +0200

    E: add custom row swipe actions to a List - both sides

--- 16_HotProspects/16_HotProspects/ContentView.swift
+++ 16_HotProspects/16_HotProspects/ContentView.swift
@@ -8,30 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
-    
     var body: some View {
-        VStack {
-            Text("Hello, world")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change Color")
-                .padding()
-                .contextMenu {
-                    Button(role: .destructive) {        // not actually destructive but makes the btn red
-                        backgroundColor = .red
+        List {
+            Text("Laura")
+                .swipeActions {
+                    Button(role: .destructive) {
+                        print("Deleting")
                     } label: {
-                        Label("Red", systemImage: "checkmark.circle.fill")
+                        Label("Delete message", systemImage: "minus.circle")
                     }
-                    
-                    Button("Green") {
-                        backgroundColor = .green
+                }
+                .swipeActions(edge: .leading) {
+                    Button {
+                        print("Pinning")
+                    } label: {
+                        Label("Pin", systemImage: "pin")
                     }
-                    
-                    Button("Blue") {
-                        backgroundColor = .blue
-                    }
+                    .tint(.orange)
                 }
         }
     }
