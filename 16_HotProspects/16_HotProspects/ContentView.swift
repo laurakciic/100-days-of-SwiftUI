f0f6ebf15958f2db59c9daede1511ff1de4c1dcd
Laura <laurakovacic@outlook.com>
2022-10-11 09:50:14 +0200

    E: control image interpolation

--- 16_HotProspects/16_HotProspects/ContentView.swift
+++ 16_HotProspects/16_HotProspects/ContentView.swift
@@ -8,39 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
-    
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    
-    func fetchReadings() async {
-        let fetchTask = Task { () -> String in                                      // takes no values, returns a string
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings."
-        }
-        
-        let result = await fetchTask.result                                         // read result, await bc it might need to wait for result come in
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Download error: \(error.localizedDescription)"
-        }
-        
-        // or
-        
-//        do {
-//            output = try result.get()
-//        } catch {
-//            print("Download error.")
-//        }
+        Image("example")
+            .interpolation(.none)                   // useful for QR codes bc we stretch them and they mustn't be blurry
+            .resizable()                            // stretch to fill available space
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
 }
 
