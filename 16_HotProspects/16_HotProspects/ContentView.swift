140736d20720b7228067d0006c387ed8ab921740
Laura <laurakovacic@outlook.com>
2022-10-11 10:39:26 +0200

    set project to inital state

--- 16_HotProspects/16_HotProspects/ContentView.swift
+++ 16_HotProspects/16_HotProspects/ContentView.swift
@@ -5,21 +5,12 @@
 //  Created by Laura on 11.10.2022..
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
-    
     var body: some View {
-        Text(results)
-    }
-    
-    var results: String {
-        // pick 7 numbers from possibleNumbers arr and make them into a single string
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)                 // array of strings
-        return strings.joined(separator: ", ")                  // join strings into a single one 
+        Text("Hello, world!")
+            .padding()
     }
 }
 
