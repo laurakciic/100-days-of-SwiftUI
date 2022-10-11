e436d616423c64669f261336d5ced4bda492c55d
Laura <laurakovacic@outlook.com>
2022-10-11 10:37:36 +0200

    E: add Swift package dependency

--- 16_HotProspects/16_HotProspects/ContentView.swift
+++ 16_HotProspects/16_HotProspects/ContentView.swift
@@ -5,37 +5,21 @@
 //  Created by Laura on 11.10.2022..
 //
 
+import SamplePackage
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
-    
+    let possibleNumbers = Array(1...60)
     
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the dogs"
-                content.subtitle = "They look hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text(results)
+    }
+    
+    var results: String {
+        // pick 7 numbers from possibleNumbers arr and make them into a single string
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)                 // array of strings
+        return strings.joined(separator: ", ")                  // join strings into a single one 
     }
 }
 
