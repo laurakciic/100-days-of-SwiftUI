c28189d69a96616c8a0ee2e3201b3bed7169d3dd
Laura <laurakovacic@outlook.com>
2022-10-12 21:29:18 +0200

    push files for C6

---
+++ Challenge6_Roll_Dice/Challenge6_Roll_Dice/Challenge6_Roll_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6_Roll_DiceApp.swift
+//  Challenge6_Roll_Dice
+//
+//  Created by Laura on 12.10.2022..
+//
+
+import SwiftUI
+
+@main
+struct Challenge6_Roll_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
